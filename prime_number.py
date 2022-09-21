a4ed5bfe3c33d5864af9fca5837e459d2dc9aaa0
Pejelagartoh <igna_98_@hotmail.com>
2022-09-21 17:00:55 -0300

    Carga de archivos

---
+++ prime_number.py
@@ -0,0 +1,19 @@
+#given a number, verufy if it is a prime number
+
+
+def is_prime(num):
+    if num == 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+    if is_prime(num):
+        print(f"{num} is a prime number")
+    else:
+        print(f"{num} is not a prime number")
+        
